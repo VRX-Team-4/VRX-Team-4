86cbc59590969a64206cb2efe2c7826772c168f6
Josh <j0sh7ran@gmail.com>
2020-08-11 14:31:16 -0500

    added base script logic for family member

--- VRX/Assets/Scripts/FamilyMember.cs
+++ VRX/Assets/Scripts/FamilyMember.cs
@@ -1,13 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class FamilyMember : MonoBehaviour
+public class FamilyMember : MonoBehaviour, IInteractable
 {
+
+    #region Properties
+
+    [SerializeField] public string ObjectName;
+    [SerializeField] public GameObject InteractMenu;
+    [SerializeField] public GameObject ButtonPreFab;
+
+    private List<GameObject> InteractMenuOptionButtons;
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InteractMenuOptionButtons = new List<GameObject>();
+
+        // Instantiate Exit button for PatientBed. 
+        GameObject exitButton = Instantiate(ButtonPreFab);
+        exitButton.transform.GetChild(0).GetComponent<Text>().text = "Exit";
+        exitButton.GetComponent<Button>().onClick.AddListener(OnExitButtonClick);
+        InteractMenuOptionButtons.Add(exitButton);
+
+        // Set each button as child to SelectedObjectInteractMenu. 
+        foreach (GameObject button in InteractMenuOptionButtons)
+        {
+            button.transform.SetParent(InteractMenu.transform);
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +39,54 @@ public class FamilyMember : MonoBehaviour
     {
         
     }
+
+    #region On Hover
+
+    public HoverOutput Hover()
+    {
+        return new HoverOutput
+        {
+            ObjectName = ObjectName
+        };
+    }
+
+    #endregion
+
+    #region On Interact
+
+    public InteractionOutput Interact()
+    {
+        // Show SelectedObjectInteractMenu on Interact. 
+        InteractMenu.SetActive(true);
+
+        // Show each button on Interact. 
+        foreach (GameObject button in InteractMenuOptionButtons)
+        {
+            button.SetActive(true);
+        }
+
+        // Select first button. 
+        InteractMenuOptionButtons[0].GetComponent<Button>().Select();
+
+        return new InteractionOutput
+        {
+            ObjectName = ObjectName,
+        };
+    }
+
+    #endregion
+
+    #region On Button Click
+
+    public void OnExitButtonClick()
+    {
+        foreach (GameObject button in InteractMenuOptionButtons)
+        {
+            button.SetActive(false);
+        }
+
+        InteractMenu.SetActive(false);
+    }
+
+    #endregion
 }
